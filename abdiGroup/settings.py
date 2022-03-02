4a589b70212c803c4668aeaca5f230d14a74d45c
Navid Azimi <navidazimi1380@gmail.com>
2022-03-03 00:08:48 +0330

    changed collect static from static to assets

--- abdiGroup/settings.py
+++ abdiGroup/settings.py
@@ -127,9 +127,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'product_and_services/static',
+    BASE_DIR / 'static'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # https settings
