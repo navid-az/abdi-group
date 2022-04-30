9e4218780da02d0c9a2f1e40b89d0f438531b92c
navid <74461221+navid-az@users.noreply.github.com>
2022-04-30 14:44:44 +0430

    Update settings.py

--- abdiGroup/settings.py
+++ abdiGroup/settings.py
@@ -82,6 +82,7 @@ WSGI_APPLICATION = 'abdiGroup.wsgi.application'
 
 DATABASES = {
     'default': {
+        'HOST': '127.0.0.1'
         'ENGINE':'django.db.backends.postgresql',
         'NAME':'abdigrou_database',
         'USER':'abdigrou_navid',
