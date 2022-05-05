85271d2634adb36d53863bdedd1a0aab9b68c5fe
Navid Azimi <navidazimi1380@gmail.com>
2022-05-05 18:40:34 +0430

    added footer

--- abdiGroup/urls.py
+++ abdiGroup/urls.py
@@ -1,6 +1,3 @@
-from argparse import Namespace
-from os import name
-from django import urls
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.i18n import i18n_patterns
