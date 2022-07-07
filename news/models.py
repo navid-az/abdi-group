a58fd4f4fdaa8301dfd59007f0d9e569ec2be25d
Navid Azimi <navidazimi1380@gmail.com>
2022-07-07 16:57:49 +0430

    deleted ck editor

--- news/models.py
+++ news/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-from ckeditor.fields import RichTextField
+# from ckeditor.fields import RichTextField
 from jalali_date import datetime2jalali, date2jalali
 
 class News(models.Model):
   title = models.CharField(max_length=250, default='hello')
-  summery = RichTextField()
+  summery = models.TextField()
   main_image = models.ImageField(upload_to='news-images/')
-  body = RichTextField()
+  body = models.TextField()
   created = models.DateTimeField(auto_now=True)
 
   def __str__(self):
